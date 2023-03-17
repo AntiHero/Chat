8c97bb13083744f1c707a6518b8c920921add6bf
Aleksandr Schemelev <prophreaker@gmail.com>
2023-03-17 20:07:47 +0300

    refactor(wrap.ts): remove unused parameter 'ok' from wrap function signature and simplify the function logic

--- packages/server/src/@common/utils/wrap.ts
+++ packages/server/src/@common/utils/wrap.ts
@@ -1,6 +1,6 @@
 import { Result } from '../types';
 
-export const wrap = <T>(valueOrError: T | Error, ok = true): Result<T, Error> =>
-  ok
+export const wrap = <T>(valueOrError: T | Error): Result<T, Error> =>
+  !(valueOrError instanceof Error)
     ? { ok: true, value: valueOrError as T }
     : { ok: false, error: valueOrError as Error };
