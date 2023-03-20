0d223ec02a9217a198286278c8977521c3e81ddc
Aleksandr Schemelev <prophreaker@gmail.com>
2023-03-21 00:39:01 +0300

    refactor: move AuthController to api folder

--- packages/server/src/auth/auth.module.ts
+++ packages/server/src/auth/auth.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 
-import { AuthController } from './auth.controller';
+import { AuthService } from './application/services/auth.service';
+import { AuthController } from './api/auth.controller';
 
 @Module({
   controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AuthModule {}
