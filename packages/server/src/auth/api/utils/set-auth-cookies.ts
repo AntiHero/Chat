fbb902fe203487ff1b22e5bfab2e564413bec3a1
Aleksandr Schemelev <prophreaker@gmail.com>
2023-03-22 23:18:13 +0300

    refactor(auth.controller.ts, set-auth-cookies.ts): fix code formatting and add OPTIONS constant for cookie options

--- packages/server/src/auth/api/utils/set-auth-cookies.ts
+++ packages/server/src/auth/api/utils/set-auth-cookies.ts
@@ -1,19 +1,17 @@
 import { type FastifyReply } from 'fastify';
 
+const OPTIONS = {
+  secure: true,
+  httpOnly: true,
+  sameSite: true,
+};
+
 export const setAuthCookies = (
   response: FastifyReply,
   accessToken: string,
   refreshToken: string,
 ) => {
   response
-    .setCookie('accessToken', accessToken, {
-      secure: true,
-      httpOnly: true,
-      sameSite: true,
-    })
-    .setCookie('refreshToken', refreshToken, {
-      secure: true,
-      httpOnly: true,
-      sameSite: true,
-    });
+    .setCookie('accessToken', accessToken, OPTIONS)
+    .setCookie('refreshToken', refreshToken, OPTIONS);
 };
